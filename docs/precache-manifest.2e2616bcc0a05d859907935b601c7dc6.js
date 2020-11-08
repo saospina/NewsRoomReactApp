dcfb7d7282c304588a1b38d0a246dab023293646
saospina <sergos0617@gmail.com>
2020-11-08 16:14:27 -0500

    adding docs

---
+++ docs/precache-manifest.2e2616bcc0a05d859907935b601c7dc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33223544824013415f38dd98e47690f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8aa7bf4879830e97f01",
+    "url": "/static/css/main.f944a772.chunk.css"
+  },
+  {
+    "revision": "acf63d3c64f0f740d7dd",
+    "url": "/static/js/2.9c9b5ac2.chunk.js"
+  },
+  {
+    "revision": "f406aedd96f61bd83ab19677f9afa536",
+    "url": "/static/js/2.9c9b5ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8aa7bf4879830e97f01",
+    "url": "/static/js/main.ccf1a7f2.chunk.js"
+  },
+  {
+    "revision": "6d9f2730d60eb208c0c5",
+    "url": "/static/js/runtime-main.293c2cc4.js"
+  },
+  {
+    "revision": "9d289f4c19c58af342399530d4559df8",
+    "url": "/static/media/oops.9d289f4c.png"
+  }
+]);
